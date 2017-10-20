141f300a99bd0a15023a633f0454d924d5efb630
binfeng.yang <binfeng.yang@hypereal.com>
2017-10-20 11:47:35 +0800

    test sh

--- script/deploy.sh
+++ script/deploy.sh
@@ -2,5 +2,8 @@
 LASTRUNER=`docker ps -q -f name=$PACKAGENAME`
 if [ -n "$LASTRUNER" ]; then
     docker kill $LASTRUNER
+    echo "kill $LASTRUNER"
 fi
+
 docker run --rm --name $PACKAGENAME -p $RUNPORT:8080 $PACKAGENAME:$BUILD_TAG
+echo "run $PACKAGENAME:$BUILD_TAG"
